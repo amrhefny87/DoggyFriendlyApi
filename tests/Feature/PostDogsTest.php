7cc396f6812fd6cc0247054652c67fc3f65ac6a2
Cris Mouta <crismouta@gmail.com>
2021-07-15 08:55:15 +0200

    se aprueban los tests de crud postdogs

--- tests/Feature/PostDogsTest.php
+++ tests/Feature/PostDogsTest.php
@@ -15,70 +15,67 @@ class PostDogsTest extends TestCase
      *
      * @return void
      */
+    
+    public function test_CheckIfPostDogsAreListedInJsonFile()
+    {
+        PostDog::factory(2)->create();
+        $response = $this->get('/api/postdogs');
+
+        $response->assertStatus(200)
+                ->assertJsonCount(2);
+    }
+
     public function test_APostDogCanBeCreated ()
     {
         $this->withoutExceptionHandling();
 
         $response = $this->post('/api/postdogs' , [
-
             "title" => "test title",
             "description"=> "chris is the coder wonderfull",
             "date" => "1991-10-11 11:31:25",
             "name" => "Hannah Haley I",
             "comments"=> "el futuro es para nosotros",
-            "image" => "amr.png"]);
+            "image" => "amr.png"
+        ]);
 
-            $response->assertOk();
-            $this->assertCount(1, PostDog::all());
+        $response->assertOk();
+        $this->assertCount(1, PostDog::all());
 
-            $this->assertEquals(PostDog::first()->title,'test title');
-            $this->assertEquals(PostDog::first()->image,'amr.png');
-
-
-
-    }
-
-    public function test_CheckIfPostDogsAreListedInJsonFile()
-    {
-       PostDog::factory(2)->create();
-       $response = $this->get('/api/postdogs');
-
-       $response->assertStatus(200)
-                ->assertJsonCount(2);
+        $this->assertEquals(PostDog::first()->title,'test title');
+        $this->assertEquals(PostDog::first()->image,'amr.png');
 
     }
-
 
     public function test_CheckIfPostDogCanBeDeleted(){
+    
         PostDog::factory(1)->create([
-            'id'=>1
-
+            'id'=> 1
         ]);
 
         $response = $this->get('/api/postdogs');
 
         $this->assertCount(1, PostDog::all());
+        
         $response = $this->delete('/api/postdogs/1');
         $this->assertCount(0, PostDog::all());
-
 
     }
 
     public function test_CheckIfPostDogCanBeUpdated(){
-       PostDog::factory(1)->create([
-            'id'=>1,
-
+        PostDog::factory(1)->create([
+            'id'=> 1,
             'title' => 'thierno'
-
         ]);
 
-         $response = $this->put('/api/postdogs/1',[
+        $response = $this->put('/api/postdogs/1', [
             "title" => "test title",
-            "description"=> "chris is the coder wonderfull",
+            "description"=> "test description",
             "date" => "1991-10-11 11:31:25",
             "name" => "Hannah Haley I",
             "comments"=> "el futuro es para nosotros",
-            "image" => "amr.png"]);
+            "image" => "image.png"
+        ]);
+        
         $this->assertEquals(PostDog::first()->title,'test title');
 
     }
